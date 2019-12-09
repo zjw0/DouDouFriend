ecd3f86380a015451f851f6b5a7d289fbb1f1ef0
jack_zhao <1104156701@qq.com>
2019-12-09 18:22:22 +0800

    完善第一模块

--- app/src/main/java/cn/appoa/doudoufriend/ui/first/FirstFragment.java
+++ app/src/main/java/cn/appoa/doudoufriend/ui/first/FirstFragment.java
@@ -1,7 +1,6 @@
 package cn.appoa.doudoufriend.ui.first;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,13 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.necer.calendar.BaseCalendar;
 import com.necer.calendar.MonthCalendar;
+import com.necer.enumeration.MultipleNumModel;
+import com.necer.enumeration.SelectedModel;
 import com.necer.listener.OnCalendarChangedListener;
 import com.necer.listener.OnCalendarMultipleChangedListener;
+import com.necer.listener.OnCalendarStateChangedListener;
 
+import org.joda.time.DurationFieldType;
 import org.joda.time.LocalDate;
 import org.litepal.LitePal;
 
@@ -30,6 +32,7 @@ import cn.appoa.doudoufriend.R;
 import cn.appoa.doudoufriend.base.BaseFragment;
 import cn.appoa.doudoufriend.db.MonthDate;
 
+
 /**
  * 首页
  */
@@ -43,6 +46,14 @@ public class FirstFragment extends BaseFragment {
     TextView tvSunDate;
     @BindView(R.id.tv_moon_date)
     TextView tvMoonDate;
+    @BindView(R.id.tv_today)
+    TextView tvToday;
+    @BindView(R.id.tv_start_date)
+    TextView tvStartDate;
+    @BindView(R.id.tv_days)
+    TextView tvDays;
+    @BindView(R.id.tv_end_date)
+    TextView tvEndDate;
     @BindView(R.id.calendar)
     MonthCalendar calendar;
     @BindView(R.id.tv_confirm_add)
@@ -61,18 +72,31 @@ public class FirstFragment extends BaseFragment {
 //        miui10Calendar.setSelectedMode(SelectedModel.MULTIPLE);
 //        LigaturePainter painter = new LigaturePainter(mActivity);
 //        miui10Calendar.setCalendarPainter(painter);
+        calendar.setSelectedMode(SelectedModel.MULTIPLE);
+        calendar.setMultipleNum(2, MultipleNumModel.FULL_REMOVE_FIRST);
         calendar.setOnCalendarChangedListener(new OnCalendarChangedListener() {
             @Override
             public void onCalendarChange(BaseCalendar baseCalendar, int year, int month, LocalDate localDate) {
                 tvSunDate.setText(year + "年" + month + "月");
-                tvMoonDate.setText("" + localDate);
-                AtyUtils.i("当前页面选中","是"+localDate);
+                tvMoonDate.setText("" + baseCalendar.getCalendarPainter());
+                AtyUtils.i("当前页面选中", "是" + localDate);
             }
         });
         calendar.setOnCalendarMultipleChangedListener(new OnCalendarMultipleChangedListener() {
             @Override
             public void onCalendarChange(BaseCalendar baseCalendar, int year, int month, List<LocalDate> currectSelectList, List<LocalDate> allSelectList) {
-
+                tvStartDate.setText(year + "年" + month + "月" + " 当前页面选中 " + currectSelectList.size() + "个  总共选中" + allSelectList.size() + "个");
+                if(currectSelectList.size()>=2){
+                    String startYear = String.valueOf(currectSelectList.get(0).dayOfYear());
+                    String startMonth = String.valueOf(currectSelectList.get(0).getYear());
+                    String startDay = String.valueOf(currectSelectList.get(0).getDayOfYear());
+                    String endDay = String.valueOf(currectSelectList.get(1).getDayOfYear());
+                    tvStartDate.setText(startYear + "-" + startMonth + "-" + startDay);
+                    tvEndDate.setText(endDay);
+                    //int days = calendar.getTwoDateCount(currectSelectList.get(0),currectSelectList.get(1),2);
+                    int days = baseCalendar.getChildCount();
+                    tvDays.setText(days + "天");
+                }
             }
         });
     }
@@ -83,31 +107,28 @@ public class FirstFragment extends BaseFragment {
 
     }
 
-    @OnClick({R.id.tv_confirm_add, R.id.tv_sun_date, R.id.tv_moon_date, R.id.calendar})
-    public void onClicked(View v){
+    @OnClick({R.id.tv_confirm_add, R.id.tv_sun_date, R.id.tv_moon_date, R.id.calendar, R.id.tv_today})
+    public void onClicked(View v) {
 //        Toast.makeText(mActivity, "is a click", Toast.LENGTH_SHORT).show();
         switch (v.getId()) {
             case R.id.tv_confirm_add:
-                AtyUtils.showShort(mActivity,"添加成功",false);
+                AtyUtils.showShort(mActivity, "添加成功", false);
                 break;
             case R.id.tv_sun_date:
-                AtyUtils.showShort(mActivity,"添加成功1",false);
+                //AtyUtils.showShort(mActivity, "添加成功1", false);
                 break;
             case R.id.tv_moon_date:
                 List<MonthDate> monthDates = LitePal.findAll(MonthDate.class);
                 for (int i = 0; i < monthDates.size(); i++) {
                     tvMoonDate.setText(monthDates.get(i).getEndDate());
                 }
-                //AtyUtils.showShort(mActivity,"添加成功2",false);
                 break;
-            case R.id.calendar:
-                AtyUtils.showShort(mActivity,"添加成功3",false);
+            case R.id.tv_today:
+                calendar.toToday();
                 break;
-
             default:
                 break;
         }
     }
-
 
 }
